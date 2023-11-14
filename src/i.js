20f4952a7fa34942b5014d4faa85ed4bf4697c57
nikhea <imonikheaugbodaga@gmail.com>
2023-11-14 23:10:48 +0100

    commited out i.js

--- src/i.js
+++ src/i.js
@@ -1,34 +1,33 @@
-{
-    "name": "2 In 1 Men's Short Sleeve Shorts Set - Black",
-    "description": "lorem 2 luxury bags",
-    "price": 2893,
-    "quantity": 5,
-    "sold": 0,
-    "pohtos": [
-      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
-      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/3.jpg?6987",
-      "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/4.jpg?6987"
-    ],
-    "coverPhoto": "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
-    "specifications": [
-      {
-        "sku": "73829827jldsjl",
-        "productLine": "Yoo To-COD",
-        "model": "black",
-        "productCountry": "china",
-        "weight (kg)": 0.3,
-        "color": "black",
-        "mainmaterial": "polyester",
-        "careLabel": "Please see our clothing size chart and Jumia official men's clothing size chart on the product picture."
-      }
-    ],
-    "availability": {
-      "quantity": 5,
-      " deliveryDate": "Fri Apr 07 2023 12:47:02 GMT+0100 (West Africa Standard Time)"
-    },
-    "category": "fashion",
-    "subcategory": "female bags",
-    "review": "",
-    "features": ["T-shirt", "shorts", "short sleeve"]
-  }
-  
+// {
+//     "name": "2 In 1 Men's Short Sleeve Shorts Set - Black",
+//     "description": "lorem 2 luxury bags",
+//     "price": 2893,
+//     "quantity": 5,
+//     "sold": 0,
+//     "pohtos": [
+//       "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+//       "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/3.jpg?6987",
+//       "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/4.jpg?6987"
+//     ],
+//     "coverPhoto": "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/68/202776/1.jpg?0636",
+//     "specifications": [
+//       {
+//         "sku": "73829827jldsjl",
+//         "productLine": "Yoo To-COD",
+//         "model": "black",
+//         "productCountry": "china",
+//         "weight (kg)": 0.3,
+//         "color": "black",
+//         "mainmaterial": "polyester",
+//         "careLabel": "Please see our clothing size chart and Jumia official men's clothing size chart on the product picture."
+//       }
+//     ],
+//     "availability": {
+//       "quantity": 5,
+//       " deliveryDate": "Fri Apr 07 2023 12:47:02 GMT+0100 (West Africa Standard Time)"
+//     },
+//     "category": "fashion",
+//     "subcategory": "female bags",
+//     "review": "",
+//     "features": ["T-shirt", "shorts", "short sleeve"]
+//   }
