ebbbcc8e346c98c9a7b06d3410956852cc47a810
nikhea <imonikheaugbodaga@gmail.com>
2024-02-17 01:29:22 +0100

    wip: stripe webhooks

--- src/routes/stripeHook.routes.js
+++ src/routes/stripeHook.routes.js
@@ -1,11 +1,9 @@
 import express from "express";
 
 import { webHook } from "../controllers/stripe.controller.js";
-import { loginRequired } from "../middleware/authtication.js";
-import { ensureCustomer } from "../middleware/roleValidation.js";
 
 const router = express.Router();
 
-router.post("/", webHook);
+router.post("/webhook", webHook);
 export default router;
 // express.raw({ type: "application/json" })
